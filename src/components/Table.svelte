18ff7d5dd07959ad3d977ce848d34727265db1dd
abir2010 <kaiserabir222@gmail.com>
2024-11-19 15:54:20 +0600

    list-grid view featur added

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -1,18 +1,28 @@
 <script>
 	import {
 		Button,
+		Card,
 		Dropdown,
 		Modal,
 		Progressbar,
 		Radio,
+		TabItem,
 		Table,
 		TableBody,
 		TableBodyCell,
 		TableBodyRow,
 		TableHead,
-		TableHeadCell
+		TableHeadCell,
+		Tabs
 	} from 'flowbite-svelte';
-	import { AdjustmentsVerticalSolid, ChevronDownOutline, LinkOutline } from 'flowbite-svelte-icons';
+	import {
+		AdjustmentsVerticalSolid,
+		ArrowUpRightFromSquareOutline,
+		ChevronDownOutline,
+		GridSolid,
+		LinkOutline,
+		ListOutline
+	} from 'flowbite-svelte-icons';
 	import Loader from './Loader.svelte';
 
 	let { data } = $props();
@@ -22,11 +32,16 @@
 	let defaultModal = $state(false);
 	let selectedData = $state({});
 	let selectedStatus = $state('');
+	let selectedView = $state('list');
 
 	let filtered = $state(allData);
 
 	const calculateProgress = (success, attempt) => {
 		return Math.round((success / attempt) * 100);
+	};
+
+	const setView = (e) => {
+		selectedView = e.currentTarget.getAttribute('value');
 	};
 </script>
 
@@ -40,7 +55,21 @@
 	<div class="col-span-3 w-full">
 		<div class="mb-3 flex justify-between">
 			<!--  list/grid view -->
-			<div>List/Grid</div>
+			<Tabs
+				tabStyle="full"
+				defaultClass="flex rounded-lg divide-x rtl:divide-x-reverse divide-gray-200 shadow"
+			>
+				<TabItem class="w-full" onclick={setView} value="list" open>
+					<span slot="title">
+						<ListOutline />
+					</span>
+				</TabItem>
+				<TabItem class="w-full" onclick={setView} value="grid">
+					<span slot="title">
+						<GridSolid />
+					</span>
+				</TabItem>
+			</Tabs>
 			<!-- Status Dropdown -->
 			<div>
 				<Button
@@ -83,81 +112,132 @@
 			</div>
 		</div>
 		<!-- Data Table -->
-		<Table class="w-full table-auto border border-gray-200 text-left text-[14px]">
-			<TableHead class="h-[50px] bg-gray-50 uppercase">
-				<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Full Name</TableHeadCell>
-				<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Location Name</TableHeadCell>
-				<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Region</TableHeadCell>
-				<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Details</TableHeadCell>
-				<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Success Rate</TableHeadCell>
-				<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Wikipedia Link</TableHeadCell>
-				<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Status</TableHeadCell>
-			</TableHead>
-			<TableBody class="divide-y">
+		{#if selectedView === 'list'}
+			<Table class="w-full table-auto border border-gray-200 text-left text-[14px]">
+				<TableHead class="h-[50px] bg-gray-50 uppercase">
+					<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Full Name</TableHeadCell>
+					<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Location Name</TableHeadCell>
+					<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Region</TableHeadCell>
+					<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Details</TableHeadCell>
+					<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Success Rate</TableHeadCell>
+					<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Wikipedia Link</TableHeadCell
+					>
+					<TableHeadCell class="px-4 py-2 font-semibold text-gray-500">Status</TableHeadCell>
+				</TableHead>
+				<TableBody class="divide-y">
+					{#each filtered as pad}
+						<TableBodyRow class="h-[50px] font-semibold hover:bg-gray-100">
+							<TableBodyCell class="px-4 py-2 text-gray-900">{pad.full_name}</TableBodyCell>
+							<TableBodyCell class="px-4 py-2 text-gray-900">{pad.location.name}</TableBodyCell>
+							<TableBodyCell class="px-4 py-2 text-gray-900">{pad.location.region}</TableBodyCell>
+							<TableBodyCell class="px-4 py-2">
+								<button
+									onclick={() => ((defaultModal = true), (selectedData = pad))}
+									class="rounded-md bg-gray-200 px-2 py-0.5 text-gray-900"
+								>
+									<span class="text-[12px]">View Details</span>
+								</button>
+							</TableBodyCell>
+							<TableBodyCell class="px-4 py-2 text-gray-900">
+								{#if pad.successful_landings !== 0}
+									<div>
+										<Progressbar
+											progress={calculateProgress(pad.successful_landings, pad.attempted_landings)}
+											progressClass="bg-green-400"
+											size="h-1.5"
+										/>
+										<span class="text-xs text-gray-500">
+											{calculateProgress(pad.successful_landings, pad.attempted_landings)}%
+										</span>
+									</div>
+								{:else}
+									<span class="text-xs text-gray-500">N/A</span>
+								{/if}
+							</TableBodyCell>
+							<TableBodyCell class="px-4 py-2">
+								<a
+									href={pad.wikipedia}
+									target="_blank"
+									rel="noopener noreferrer"
+									class="text-blue-600"
+								>
+									<LinkOutline class="h-5 w-5 text-blue-700" />
+								</a>
+							</TableBodyCell>
+							<TableBodyCell class="px-4 py-2">
+								{#if pad.status === 'active'}
+									<span
+										class="inline-flex items-center rounded-md bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-800"
+									>
+										Active
+									</span>
+								{:else if pad.status === 'retired'}
+									<span
+										class="inline-flex items-center rounded-md bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-800"
+									>
+										Retired
+									</span>
+								{:else}
+									<span
+										class="inline-flex items-center rounded-md bg-blue-100 px-2.5 py-0.5 text-xs font-medium text-blue-800"
+									>
+										Under Construction
+									</span>
+								{/if}
+							</TableBodyCell>
+						</TableBodyRow>
+					{/each}
+				</TableBody>
+			</Table>
+		{:else}
+			<div class="grid w-full grid-cols-1 lg:grid-cols-3 lg:gap-3">
 				{#each filtered as pad}
-					<TableBodyRow class="h-[50px] font-semibold hover:bg-gray-100">
-						<TableBodyCell class="px-4 py-2 text-gray-900">{pad.full_name}</TableBodyCell>
-						<TableBodyCell class="px-4 py-2 text-gray-900">{pad.location.name}</TableBodyCell>
-						<TableBodyCell class="px-4 py-2 text-gray-900">{pad.location.region}</TableBodyCell>
-						<TableBodyCell class="px-4 py-2">
+					<Card class="col-span-1">
+						<div>
+							<div class="flex justify-end">
+								{#if pad.status === 'active'}
+									<span
+										class="inline-flex items-center rounded-md bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-800"
+									>
+										Active
+									</span>
+								{:else if pad.status === 'retired'}
+									<span
+										class="inline-flex items-center rounded-md bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-800"
+									>
+										Retired
+									</span>
+								{:else}
+									<span
+										class="inline-flex items-center rounded-md bg-blue-100 px-2.5 py-0.5 text-xs font-medium text-blue-800"
+									>
+										Under Construction
+									</span>
+								{/if}
+							</div>
+							<h5 class="mb-2 text-lg font-semibold tracking-tight text-gray-900 dark:text-white">
+								{pad.full_name}
+							</h5>
+							<h6 class="text-xs text-gray-500">{pad.location.name}, {pad.location.region}</h6>
+						</div>
+						<div class="mt-4 flex justify-between">
 							<button
 								onclick={() => ((defaultModal = true), (selectedData = pad))}
 								class="rounded-md bg-gray-200 px-2 py-0.5 text-gray-900"
 							>
 								<span class="text-[12px]">View Details</span>
 							</button>
-						</TableBodyCell>
-						<TableBodyCell class="px-4 py-2 text-gray-900">
-							{#if pad.successful_landings !== 0}
-								<div>
-									<Progressbar
-										progress={calculateProgress(pad.successful_landings, pad.attempted_landings)}
-										progressClass="bg-green-400"
-										size="h-1.5"
-									/>
-									<span class="text-xs text-gray-500">
-										{calculateProgress(pad.successful_landings, pad.attempted_landings)}%
-									</span>
-								</div>
-							{:else}
-								<span class="text-xs text-gray-500">N/A</span>
-							{/if}
-						</TableBodyCell>
-						<TableBodyCell class="px-4 py-2">
 							<a
 								href={pad.wikipedia}
-								target="_blank"
-								rel="noopener noreferrer"
-								class="text-blue-600"
+								class="inline-flex items-center text-sm text-blue-500 hover:underline"
 							>
-								<LinkOutline class="h-5 w-5 text-blue-700" />
+								<ArrowUpRightFromSquareOutline class="ms-2.5 h-4 w-4" />
 							</a>
-						</TableBodyCell>
-						<TableBodyCell class="px-4 py-2">
-							{#if pad.status === 'active'}
-								<span
-									class="inline-flex items-center rounded-md bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-800"
-								>
-									Active
-								</span>
-							{:else if pad.status === 'retired'}
-								<span
-									class="inline-flex items-center rounded-md bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-800"
-								>
-									Retired
-								</span>
-							{:else}
-								<span
-									class="inline-flex items-center rounded-md bg-blue-100 px-2.5 py-0.5 text-xs font-medium text-blue-800"
-								>
-									Under Construction
-								</span>
-							{/if}
-						</TableBodyCell>
-					</TableBodyRow>
+						</div>
+					</Card>
 				{/each}
-			</TableBody>
-		</Table>
+			</div>
+		{/if}
 	</div>
 
 	<!-- Details Modal -->
